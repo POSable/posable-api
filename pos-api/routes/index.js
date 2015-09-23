985544b4e909d845e0be973ba82e697cfaf8800b
Tristan Rubadeau <tristan@rubadeau.com>
2015-09-23 16:57:58 -0600

    current time returning to index

--- pos-api/routes/index.js
+++ pos-api/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var currentdate = new Date();
+var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { datetime: datetime });
 });
 
 module.exports = router;
