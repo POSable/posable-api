3c9e81be788b149e789cb45a553af1ecc14bc239
Nick Peters <nick@barbetsapp.com>
2015-09-28 12:42:48 -0600

    change back to tasks, insert into get call

--- pos-api/routes/index.js
+++ pos-api/routes/index.js
@@ -8,47 +8,49 @@ router.get('/', function(req, res, next) {
   var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
   res.render('index', { datetime: datetime });
   log.info('User requested time: ' + datetime);
+
+  var q = 'tasks';
+
+  function bail(err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  // Publisher
+  function publisher(conn) {
+    conn.createChannel(on_open);
+    function on_open(err, ch) {
+      if (err != null) bail(err);
+      ch.assertQueue(q);
+      ch.sendToQueue(q, new Buffer('something to do'));
+      console.log("sending something to do");
+      setTimeout(consumer(conn), 1000); // remove - used for simple test - infinite loop with consumer function!!!
+    }
+  }
+
+   //Consumer
+  function consumer(conn) {
+    conn.createChannel(on_open);
+    function on_open(err, ch) {
+      if (err != null) bail(err);
+      ch.assertQueue(q);
+      ch.consume(q, function(msg) {
+        if (msg !== null) {
+          console.log("message content is", msg.content.toString());
+          ch.ack(msg);
+        }
+      });
+    }
+  }
+
+  require('amqplib/callback_api')
+      .connect('amqp://52.89.7.38:5672', function(err, conn) {
+        if (err != null) bail(err);
+        consumer(conn);
+        publisher(conn); // remove - used for simple test
+      });
+
 });
 
-var q = 'steve';
-
-function bail(err) {
-  console.error(err);
-  process.exit(1);
-}
-
-// Publisher
-function publisher(conn) {
-  conn.createChannel(on_open);
-  function on_open(err, ch) {
-    if (err != null) bail(err);
-    ch.assertQueue(q);
-    ch.sendToQueue(q, new Buffer('something to do'));
-    console.log("sending something to do");
-    setTimeout(consumer(conn), 1000); // remove - used for simple test - infinite loop with consumer function!!!
-  }
-};
-
- //Consumer
-function consumer(conn) {
-  conn.createChannel(on_open);
-  function on_open(err, ch) {
-    if (err != null) bail(err);
-    ch.assertQueue(q);
-    ch.consume(q, function(msg) {
-      if (msg !== null) {
-        console.log("message content is", msg.content.toString());
-        ch.ack(msg);
-      }
-    });
-  }
-}
-
-require('amqplib/callback_api')
-    .connect('amqp://52.89.7.38:5672', function(err, conn) {
-      if (err != null) bail(err);
-      consumer(conn);
-      publisher(conn); // remove - used for simple test
-    });
 
 module.exports = router;
