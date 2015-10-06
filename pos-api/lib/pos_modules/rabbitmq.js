823c6aefcab571107ff5cfd49947e87cdf69a930
David Abramowitz <david.xesllc@gmail.com>
2015-10-06 14:58:49 -0600

    Rabbit Class with DI - Maybe

--- pos-api/lib/pos_modules/rabbitmq.js
+++ pos-api/lib/pos_modules/rabbitmq.js
@@ -1,45 +1,47 @@
-var amqp = require('amqplib/callback_api')
+
 
     function bail(err) {
         console.error(err);
         process.exit(1);
     }
 
-var RabbitMQ = function (urlPort, q) {
+var RabbitMQ = function (stringObject, urlPort, q, amqp) {
     this.urlPort = urlPort;
     this.q = q;
-
+    this.amqp = amqp;
+    this.stringObject = stringObject;
 };
-        RabbitMQ.prototype.publish = function () {
-            amqp.connect('amqp://' + this.urlPort, function (err, conn) {
-                    if (err != null) bail(err);
-                conn.createChannel(on_open);
-                function on_open(err, ch) {
-                    if (err != null) bail(err);
-                    ch.assertQueue(this.q);
-                    ch.sendToQueue(this.q, new Buffer('something to do'));
-                    console.log("publishing", "something to do");
-                }
-            });
-        }
 
-        RabbitMQ.prototype.consume = function () {
-            amqp.connect('amqp://' + this.urlPort, function (err, conn) {
-                if (err != null) bail(err);
-                conn.createChannel(on_open);
-                function on_open(err, ch) {
-                    if (err != null) bail(err);
-                    ch.assertQueue(this.q);
-                    ch.consume(this.q, function (msg) {
-                        if (msg !== null) {
-                            console.log("consuming message content", msg.content.toString());
-                            ch.ack(msg);
-                        } else {
-                            console.log("null dude!!")
-                        }
-                    });
+RabbitMQ.prototype.publish = function () {
+    var that = this;
+    this.amqp.connect('amqp://' + this.urlPort, function (err, conn) {
+        if (err != null) bail(err);
+        conn.createChannel(on_open);
+        function on_open(err, ch) {
+            if (err != null) bail(err);
+            ch.assertQueue(that.q);
+            ch.sendToQueue(that.q, new Buffer(that.stringObject));
+            console.log("publishing", that.stringObject);
+        }
+    });
+}
+
+RabbitMQ.prototype.consume = function () {
+    var that = this;
+    this.amqp.connect('amqp://' + this.urlPort, function (err, conn) {
+        if (err != null) bail(err);
+        conn.createChannel(on_open);
+        function on_open(err, ch) {
+            if (err != null) bail(err);
+            ch.assertQueue(that.q);
+            ch.consume(that.q, function (msg) {
+                if (msg !== null) {
+                    console.log("consuming message content", msg.content.toString());
+                    ch.ack(msg);
                 }
             });
         }
+    });
+}
 
 module.exports = RabbitMQ;
