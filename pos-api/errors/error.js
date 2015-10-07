5d25a16c1b841d691b5cc2dfde6a9249c78b9a04
Tristan Rubadeau <spartacus@Tristans-MacBook-Pro.local>
2015-10-07 12:55:52 -0600

    created standard 200, 300, 400 error library

---
+++ pos-api/errors/error.js
@@ -0,0 +1,78 @@
+
+{
+  "code" : 200,
+  "message" : "Successful",
+  "description" : "No resource creation"
+}
+
+{
+  "code" : 201,
+  "message" : "Successfully Created",
+  "description" : "location of new resource here"
+}
+
+{
+  "code" : 204,
+  "message" : "Successful",
+  "description" : "No Content"
+}
+
+{
+  "code" : 304,
+  "message" : "Not Modified",
+  "description" : "Something bad happened"
+}
+
+{
+  "code" : 400,
+  "message" : "Bad Request",
+  "description" : "More details about the error here"
+}
+
+{
+  "code" : 401,
+  "message" : "Unauthorized",
+  "description" : "More details about the error here"
+}
+
+{
+  "code" : 403,
+  "message" : "Forbidden",
+  "description" : "More details about the error here"
+}
+
+{
+  "code" : 404,
+  "message" : "Not Found",
+  "description" : "More details about the error here"
+}
+
+{
+  "code" : 405,
+  "message" : "Method Not Allowed",
+  "description" : "More details about the error here"
+}
+
+{
+  "code" : 410,
+  "message" : "Gone",
+  "description" : "Resource no longer available"
+}
+
+{
+  "code" : 415,
+  "message" : "Unsupported Media Type",
+  "description" : "More details about the error here"
+}
+
+{
+  "code" : 422,
+  "message" : "Unprocessable Entity",
+  "description" : "Validation Error - More details about the error here"
+}
+
+{
+  "code" : 429,
+  "message" : "Too Many Requests",
+  "description" : "Rejected due to Rate Limit"
+}
