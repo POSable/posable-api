5dd53d7b15960d18eec76b19c464f162271b5266
Tristan Rubadeau <tristan@rubadeau.com>
2015-10-13 15:13:44 -0600

    refectored POST action

--- pos-api/routes/transactions.js
+++ pos-api/routes/transactions.js
@@ -17,6 +17,18 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
 
+  if (req.headers.token === null || req.headers.token === undefined ){
+      var errAuth1 = new Error();
+      errAuth1.message = "Unauthorized, token is missing";
+      res.json({error: errAuth1.message, file: errAuth1.fileName, line: errAuth1.lineNumber, code: 401});
+  } else if (req.headers.token === token) {
+      res.json({message: 'You are here & token matched', code: res.statusCode});
+  } else {
+      var errAuth2 = new Error();
+      errAuth2.message = "Unauthorized, incorrect token";
+      res.json({error: errAuth2.message, file: errAuth2.fileName, line: errAuth2.lineNumber, code: 401});
+  }
+
     var transaction = new Transaction();
 
     transaction.cardType = req.body.cardType;
@@ -34,19 +46,9 @@ router.post('/', function(req, res) {
         if (err) { return next(err) }
         res.json(201, post)
     })
-})
-
-    if (req.headers.token === null || req.headers.token === undefined ){
-        var errAuth1 = new Error();
-        errAuth1.message = "Unauthorized, token is missing";
-        res.json({error: errAuth1.message, file: errAuth1.fileName, line: errAuth1.lineNumber, code: 401});
-    } else if (req.headers.token === token) {
-        res.json({message: 'You are here & token matched', code: res.statusCode});
-    } else {
-        var errAuth2 = new Error();
-        errAuth2.message = "Unauthorized, incorrect token";
-        res.json({error: errAuth2.message, file: errAuth2.fileName, line: errAuth2.lineNumber, code: 401});
-    }
 });
+
+
+
 
 module.exports = router;
