c417a9335225604f4851288960eff7a61660c57f
David Abramowitz <david.xesllc@gmail.com>
2015-10-20 14:53:38 -0600

    Finished Wire and Test

--- pos-api/lib/pos_modules/DTO.js
+++ pos-api/lib/pos_modules/DTO.js
@@ -1,3 +1,19 @@
+var o2x = require('object-to-xml');
+
+var sendError  = function (res, message, status) {
+    res.status(status);
+    if (res.req.headers['content-type'] === 'application/xml') {
+        res.send(o2x({
+            '?xml version="1.0" encoding="utf-8"?' : null,
+            error: message,
+        }));
+    } else {
+        res.json({
+            error: message,
+        });
+    }
+};
+
 var DTO = function (req, res) {
     this.req = req;
     this.res = res;
@@ -6,32 +22,20 @@ var DTO = function (req, res) {
 
     DTO.prototype.getReqBody = function () {
         var requestBody = this.req.body;
+        var message = ""
 
         if  (requestBody === {}) {
-            var errDTO1 = new Error();
-            errDTO1.message = "Request Body Error";
-            this.res.status(406);
-            this.res.json({error: errDTO1.message, code: 406});
+            message = "Request Body Error";
+            sendError(this.res, message, 406);
             return false;
-
-        } else if (this.req.headers['content-type'] === "application/json") {
+        } else if (this.req.headers['content-type'] === "application/json"  || this.req.headers['content-type'] === "application/xml" ) {
             this.dto = requestBody;
-            console.log('JSON', this.dto);
-            return true;
-        }
-        else if (this.req.headers['content-type'] === "application/xml" ) {
-            this.res.set('Content-Type', 'application/xml');
-            this.res =
-            this.dto = requestBody;
-            console.log('XML', this.dto);
             return true;
         } else {
-            console.log(this.req.headers['content-type']);
-            this.res.status(406);
-            this.res.json({error: "Content-Type Error", code: 406});
+            message = "Content-Type Error";
+            sendError(this.res, message, 406);
             return false;
         }
-
     }
 
     DTO.prototype.dto = function () {
--- pos-api/routes/transactions.js
+++ pos-api/routes/transactions.js
@@ -5,7 +5,7 @@ var Oauth = require ('../lib/pos_modules/Oauth');
 var uid = require('rand-token').uid;
 var DTO = require('../lib/pos_modules/DTO');
 var map = require('../lib/pos_modules/transactionMap');
-var Transaction = require('../models/transaction.js');
+var Transaction = require('../models/transaction');
 var transaction = new Transaction();
 var o2x = require('object-to-xml');
 var Val = require('../lib/pos_modules/validation');
@@ -21,40 +21,51 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function(req, res) {
+    console.log("Post received with content type of ", req.headers['content-type'])
 
     var newAuth = new Oauth(req, res, uid);
     if (!newAuth.authenticatePost()) return;
-    console.log("finished auth");
+    console.log("finished Oauth");
 
     var transactionDTO = new DTO(req, res);
     if (!transactionDTO.getReqBody()) return;
     console.log("finished DTO");
 
-    if (!map(transactionDTO.dto, transaction)) return;
-    console.log("mapped DTO to Transaction");
+    if (!map(transactionDTO.dto, transaction, res)) return;
+    console.log("finished mapping DTO to Transaction");
 
     var newVal = new Val(req, res, transactionDTO);
     if (!newVal.paymentValidator()) return;
     console.log("finished Validations")
 
     transaction.save(function (err, post) {
-        //if (err) { return next(err) }
+        if (err) {
+            var message = "DB Error";
+            res.status(500);
+            if (res.req.headers['content-type'] === 'application/xml') {
+                res.send(o2x({
+                    '?xml version="1.0" encoding="utf-8"?' : null,
+                    error: message,
+                }));
+            } else {
+                res.json({
+                    error: message,
+                });
+            }
+        } else {
+            console.log("Posted this object to DB ", post);
+            if (res.req.headers['content-type'] === 'application/xml') {
+                res.send(o2x({
+                    '?xml version="1.0" encoding="utf-8"?' : null,
+                    message: 'POS API Transaction, token matched, validation passed, object mapped and saved in mongo',
+                }))
+            } else {
+                res.json({
+                    message: 'POS API Transaction, token matched, validation passed, object mapped and saved in mongo',
+                });
+            }
+        }
     });
-
-    if (res.req.headers['content-type'] === 'application/xml') {
-        res.send(o2x({
-            '?xml version="1.0" encoding="utf-8"?' : null,
-            message: 'You are here, token matched, validation passed, object mapped and saved in mongo',
-            code: 200
-        }))
-
-    } else {
-        res.json({
-            message: 'You are here, token matched, validation passed, object mapped and saved in mongo',
-            code: 200
-        });
-    }
-
 });
 
 
