133184049edeb0291eb4edf3c998126f86385d52
David Abramowitz <david.xesllc@gmail.com>
2015-10-22 14:56:43 -0600

    Minor Fix in PaymentSave

--- pos-api/lib/pos_modules/paymentSave.js
+++ pos-api/lib/pos_modules/paymentSave.js
@@ -6,42 +6,14 @@ var save  = function (res, payment, statusObject, callback) {
                     module: createPaymentDTO,
                     error: {message: "DB save error", errorObject: err }
                 };
+            } else {
+                statusObject.success.push("paymentSave'");
+                console.log("Posted", post);
             }
-            statusObject.success.push("paymentSave'");
-            console.log("Posted", post);
+
             callback(res, statusObject);
         })
 
 };
-
-
-//    (function (err, post) {
-//    if (err) {
-//        var message = "DB Error";
-//        res.status(500);
-//        if (res.req.headers['content-type'] === 'application/xml') {
-//            res.send(o2x({
-//                '?xml version="1.0" encoding="utf-8"?' : null,
-//                error: message,
-//            }));
-//        } else {
-//            res.json({
-//                error: message,
-//            });
-//        }
-//    } else {
-//        console.log("Posted this object to DB ", post);
-//        if (res.req.headers['content-type'] === 'application/xml') {
-//            res.send(o2x({
-//                '?xml version="1.0" encoding="utf-8"?' : null,
-//                message: 'POS API Transaction, token matched, validation passed, object mapped and saved in mongo',
-//            }))
-//        } else {
-//            res.json({
-//                message: 'POS API Transaction, token matched, validation passed, object mapped and saved in mongo',
-//            });
-//        }
-//    }
-//});
 
 module.exports = save;
