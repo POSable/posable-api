dce2c1e18f9ec8c8b6b0656ebca302f4f556379d
Tristan Rubadeau <tristan@rubadeau.com>
2015-11-09 16:36:51 -0700

    messages consuming and persisting to Mongo

--- persistence/handlers/createPersistence.js
+++ persistence/handlers/createPersistence.js
@@ -1,8 +1,26 @@
 var wascally = require('wascally');
+var mongoose = require('mongoose');
+var saveTransactionInDB = require('../lib/saveTransaction');
+var Transaction = require('../models/transaction').model;
+
+mongoose.connect('mongodb://localhost/paymentData');
 
 function createPersistence(msg) {
-    console.log( 'Received from rabbit:', JSON.stringify(msg.body) );
-    msg.ack(); }
+    var transaction = new Transaction();
+
+    transaction.transactionID = msg.body;
+
+    transaction.save(function(err) {
+         if (err) {
+             console.log('Error');
+         } else{
+             console.log( 'Transaction was saved' );
+         }
+     });
+
+    console.log( 'Received from rabbit: ', JSON.stringify(msg.body) );
+    msg.ack();
+}
 
 module.exports = createPersistence;
 
