d27e96dd2095e62f7c5d3a9fb3b42063339d717a
David Abramowitz <david.xesllc@gmail.com>
2015-11-08 23:28:12 -0700

    Wascally API Publish

---
+++ commonServiceLib/wascallyConfig.js
@@ -0,0 +1,103 @@
+var settings = {
+    connection: {
+        user: 'david.xesllc@gmail.com',
+        pass: 'deavtdc021076',
+        server: [ '52.89.7.38' ],
+        port: 5672,
+        vhost: 'dave_dev',
+    },
+
+    // define the exchanges
+    exchanges: [
+        {
+            name: 'all-commands',
+            type: 'direct',
+            autoDelete: false
+        },
+        {
+            name: 'posapi.event.receivedCreatePaymentRequest',
+            type: 'fanout',
+            autoDelete: false
+        },
+        {
+            name: 'posapi.event.receivedCreateTransactionRequest',
+            type: 'fanout',
+            autoDelete: false
+        }
+    ],
+
+    // setup the queues, only subscribing to the one this service
+    // will consume messages from
+    queues: [
+        {
+            name: 'service.posapi',
+            autoDelete: false,
+            subscribe: false //subscribeTo === 'posapi'
+        },
+        {
+            name: 'service.logging',
+            autoDelete: false,
+            subscribe: false //subscribeTo === 'logging'
+        },
+        {
+            name: 'service.persistence',
+            autoDelete: false,
+            subscribe: false //subscribeTo === 'persistence'
+        },
+        {
+            name: 'service.externalIntegration',
+            autoDelete: false,
+            subscribe: false //subscribeTo === 'externalIntegration'
+        },
+    ],
+
+    // binds exchanges and queues to one another
+    bindings: [
+        {
+            exchange: 'all-commands',
+            target: 'service.posapi',
+            keys: [ 'service.posapi' ]
+        },
+        {
+            exchange: 'all-commands',
+            target: 'service.logging',
+            keys: [ 'service.logging' ]
+        },
+        {
+            exchange: 'all-commands',
+            target: 'service.persistence',
+            keys: [ 'service.persistence' ]
+        },
+        {
+            exchange: 'all-commands',
+            target: 'service.externalIntegration',
+            keys: [ 'service.externalIntegration' ]
+        },
+        {
+            exchange: 'posapi.event.receivedCreatePaymentRequest',
+            target: 'service.persistence',
+            keys: []
+        },
+        {
+            exchange: 'posapi.event.receivedCreatePaymentRequest',
+            target: 'service.externalIntegration',
+            keys: []
+        },
+        {
+            exchange: 'posapi.event.receivedCreateTransactionRequest',
+            target: 'service.persistence',
+            keys: []
+        },
+        {
+            exchange: 'posapi.event.receivedCreateTransactionRequest',
+            target: 'service.externalIntegration',
+            keys: []
+        }
+    ]
+}
+
+
+module.exports = {
+    settings: settings,
+    loggerKey:  'service.logging'
+}
