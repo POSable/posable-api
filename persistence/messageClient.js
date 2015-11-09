40889f7b269f7adeb42b59bc43021a3aa032af7f
Tristan Rubadeau <tristan@rubadeau.com>
2015-11-09 10:40:34 -0700

    added file structure

---
+++ persistence/messageClient.js
@@ -0,0 +1,26 @@
+var wascally = require('wascally');
+var curWascally;
+var settings = require('./settings');
+var createPersistence = require('./handlers/createPersistence');
+
+function setHandlers(){
+    console.log('set handlers');
+    wascally.handle( 'service.persistence.addLogEntry', createPersistence) }
+
+function loadWascallyConfig(){
+    setHandlers();
+    console.log('configure');
+    wascally.configure(settings).done(function() {
+        curWascally = wascally; }); }
+
+function setSingleton(){
+    console.log('in messageClient');
+    if(!curWascally){
+        console.log('no connect');
+        loadWascallyConfig(); }
+    return curWascally; }
+
+
+module.exports.messageClient = function(){
+    return setSingleton();
+};
