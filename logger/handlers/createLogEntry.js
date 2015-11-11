df6a01c8dbc41c1a5caeb6707671450135520452
Nick Peters <nick@barbetsapp.com>
2015-11-11 15:12:38 -0700

    implement wascally wrapper for logging service

--- logger/handlers/createLogEntry.js
+++ logger/handlers/createLogEntry.js
@@ -1,8 +1,8 @@
-var wascally = require('wascally');
-
-function createLogEntry(msg) {
-    console.log( 'Received from rabbit:', JSON.stringify(msg.body) );
-    msg.ack(); }
-
-module.exports = createLogEntry;
+//var wascally = require('../node_modules/posable-wascally-wrapper/node_modules/wascally');
+//
+//function createLogEntry(msg) {
+//    console.log( 'Received from rabbit:', JSON.stringify(msg.body) );
+//    msg.ack(); }
+//
+//module.exports = createLogEntry();
 
