2993f2ec999f7bf9befea894e7f5093f4ad8f83b
Tristan Rubadeau <tristan@rubadeau.com>
2015-11-11 13:05:39 -0700

    fixed up env file syntax errors

--- persistence/start.js
+++ persistence/start.js
@@ -1,4 +1,13 @@
 require('./messageClient').messageClient();
 
+var wascallyRabbit = require('posable-wascally-wrapper');
+var env = require('../pos-api/lib/pos_modules/common').config();
+var func = function (msg) {console.log(msg)};
+var messageType = 'service.persistence';
 
+
+wascallyRabbit.setEnvConnectionValues(env['wascally_connection_parameters']);
+wascallyRabbit.setQSubscription('service.persistence');
+wascallyRabbit.setHandler(messageType, func);
+wascallyRabbit.setup();
 
