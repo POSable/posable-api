11d0dda9985bbe21ab821b0551ce6ba239e5248d
Nick Peters <nick@barbetsapp.com>
2015-11-18 13:25:50 -0700

    add log plugin to logger, removed subscription setup for pos-api

--- logger/handlers/createLogEntry.js
+++ logger/handlers/createLogEntry.js
@@ -1,8 +1,39 @@
-//var wascally = require('../node_modules/posable-wascally-wrapper/node_modules/wascally');
-//
-//function createLogEntry(msg) {
-//    console.log( 'Received from rabbit:', JSON.stringify(msg.body) );
-//    msg.ack(); }
-//
-//module.exports = createLogEntry();
+var Log = require('../models/log').model;
+var mongoose = require('mongoose');
+var logPlugin = require('posable-logging-plugin');
+
+var mapLogEntry = function(msg) {
+    try {
+        var logEntry = new Log();
+        logEntry.server = msg.server;
+        logEntry.application = msg.application;
+        logEntry.level = msg.logLevel;
+        logEntry.message = msg.message;
+        if (typeof(msg) == 'string') {
+            logEntry.stack = 'No Error Trace';
+        } else {
+            logEntry.stack = msg.stack;
+        }
+        return logEntry;
+
+    } catch (err) {
+        logPlugin.error(err);
+    }
+};
+
+var createLogEntry = function(msg){
+    var newLog = mapLogEntry(msg.body);
+    newLog.save(function(err) {
+        if (err) {
+            logPlugin.error(err);
+        } else {
+            console.log('Log saved successfully');
+        }
+    });
+    console.log( 'Received from rabbit: ', JSON.stringify(msg.body) );
+    msg.ack();
+};
+
+
+module.exports = createLogEntry;
 
