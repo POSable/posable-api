830c9e9a14fbdc589685b2385ebff839782dd0df
David Abramowitz <david.xesllc@gmail.com>
2015-11-19 11:18:59 -0700

    Console Log from this Service

--- pos-api/lib/pos_modules/api/mapPayment.js
+++ pos-api/lib/pos_modules/api/mapPayment.js
@@ -1,8 +1,8 @@
 var Payment = require('../../../models/payment').model;
-
+var payment = {};
 var mapPayment = function(dto, statusObject) {
     try {
-        var payment = new Payment();
+        payment = new Payment();
         payment.uid = dto.uid;
         payment.transactionID = dto.transactionID;
         payment.merchantID = dto.merchantID;
@@ -17,24 +17,7 @@ var mapPayment = function(dto, statusObject) {
             payment.last4 = dto.creditCard.last4;
             payment.authCode = dto.creditCard.authCode; }
 
-<<<<<<< HEAD
-            statusObject.success.push("mapPayment");
-
-            return payment;
-
-        } catch (error) {
-            statusObject.isOK = false;
-            statusObject['error'] = {
-                module: "paymentMap",
-                error: {message: "Payment DTO was not successfully created from Post Body"}
-            };
-            return payment;
-        }
-    };
-=======
         statusObject.success.push("mapPayment");
-
-        return payment;
 
     } catch (error) {
         statusObject.isOK = false;
@@ -45,6 +28,5 @@ var mapPayment = function(dto, statusObject) {
     }
     return payment;
 };
->>>>>>> 5201eb40d491b6d577b83d7cac50e41c25e0a0fd
 
 module.exports = mapPayment;
