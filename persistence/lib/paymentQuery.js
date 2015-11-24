1890662762354afb62aab911263d9775de20b302
Tristan Rubadeau <tristan@rubadeau.com>
2015-11-24 10:32:10 -0700

    paymentQuery structure

---
+++ persistence/lib/paymentQuery.js
@@ -0,0 +1,13 @@
+var paymentSum;
+
+var paymentQuery = function() {
+    try {
+        //console.log();
+
+    } catch (err) {
+        console.log(err);
+    }
+    return paymentSum;
+};
+
+module.exports = paymentQuery();
