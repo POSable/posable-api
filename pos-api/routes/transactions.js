74c92f2fa5ed8060abe356ac90a92e16009381f7
David Abramowitz <david.xesllc@gmail.com>
2015-12-01 13:33:57 -0700

    Collection EndPoint Created and Working

--- pos-api/routes/transactions.js
+++ pos-api/routes/transactions.js
@@ -67,7 +67,7 @@ router.post('/', function(req, res) {
             console.log("in finalize post");
             if (statusObject.responseType === 'alt') {
                 console.log("before send to rabbit")
-                wascallyRabbit.raiseErrorResponseEmailAndPersist(transactionDTO).then(sendResponse(res, statusObject), function(){
+                wascallyRabbit.raiseErrorResponseEmailAndPersist(req).then(sendResponse(res, statusObject), function(){
                     console.log("error sending to rabbit")
                     sendResponse(res, statusObject);
                 })
