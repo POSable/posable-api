0ceca56d496952309b4ff3be3438837c136d4196
David Abramowitz <david.xesllc@gmail.com>
2015-12-28 09:28:19 -0700

    Merge

--- email/start.js
+++ email/start.js
@@ -1,8 +1,10 @@
 var wascallyRabbit = require('posable-wascally-wrapper');
 var env = require('./common').config();
+console.log ("before sendMail");
 var sendMail = require('./email').sendMail;
+console.log('after sendMail')
 
 wascallyRabbit.setEnvConnectionValues(env['wascally_connection_parameters']);
 wascallyRabbit.setQSubscription('service.email');
 wascallyRabbit.setHandler('posapi.event.receivedBadApiRequest', sendMail);
-wascallyRabbit.setup();
+wascallyRabbit.setup('email');
