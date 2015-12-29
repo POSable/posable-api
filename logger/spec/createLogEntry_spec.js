56e6b875dc622067d2215c773066e8ca37019c42
Nick Peters <nick@barbetsapp.com>
2015-12-29 11:47:18 -0600

    add logger specs for bad messages

--- logger/spec/createLogEntry_spec.js
+++ logger/spec/createLogEntry_spec.js
@@ -2,6 +2,8 @@ describe('Logging service', function() {
 
     var createLogEntry = require('../handlers/createLogEntry').createLogEntry;
     var mapLogEntry = require('../handlers/createLogEntry').mapLogEntry;
+    var testSetup = require('../handlers/createLogEntry').testSetup;
+    var logPlugin;
 
 
     describe('calls mapLogEntry and assigns log.stack', function() {
@@ -16,6 +18,28 @@ describe('Logging service', function() {
             var testMsg = { body: 'testMessage' };
             var testLogMap = mapLogEntry(testMsg.body);
             expect(testLogMap.stack).toEqual('No Error Trace');
+        });
+    });
+
+    describe('catches errors', function(){
+        var testLogPlugin, testMsg;
+
+        beforeEach(function(){
+            logPlugin = {error: function(){}};
+            spyOn(logPlugin, 'error');
+            testLogPlugin = testSetup(logPlugin);
+        });
+
+        it('when mapLogEntry is passed a bad message', function(){
+            testMsg = null;
+            mapLogEntry(testMsg);
+            expect(testLogPlugin.error).toHaveBeenCalled();
+        });
+
+        it('when createLogEntry is passed a bad message', function(){
+            testMsg = {};
+            createLogEntry(testMsg);
+            expect(testLogPlugin.error).toHaveBeenCalled();
         });
     });
 
