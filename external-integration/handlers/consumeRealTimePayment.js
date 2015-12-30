cb721e98d26397810288df158b0427094f232b52
David Abramowitz <david.xesllc@gmail.com>
2015-12-30 09:35:41 -0700

    Test Handler Module

--- external-integration/handlers/consumeRealTimePayment.js
+++ external-integration/handlers/consumeRealTimePayment.js
@@ -1,26 +1,35 @@
 var post = require('../lib/cloudElementsClient');
 var configPlugin = require('posable-customer-config-plugin');
+var err = null;
 
-var handleRealTimePayment = function(msg) {
-
-    console.log(msg);
-
-    var id = msg.body.internalID;
-    console.log(id);
+var handleRealTimePayment = function(msg, callback) {
+    try {
+        var id = msg.body.internalID;
+    } catch (err) {
+        console.log('HandleRealTimePayment MSG ID Parsing', err);
+        msg.nack();
+        return callback(err);
+    }
     configPlugin.merchantLookup(id, function(err, merchant){
-
-        if (merchant.batchType == "real-time") {
-            console.log("Real-time merchant");
-            post();
-        } else {
-            console.log("Daily batch merchant");
-            //what to do here?
+        try {
+            if (err) throw err;
+            if (merchant == undefined) var merchant = {}; // for testing only
+            if (merchant.batchType === "real-time") {
+                console.log("Real-time merchant");
+                post();
+            } else {
+                console.log("Daily batch merchant");
+                //what to do here?
+            }
+        } catch (err) {
+            console.log('HandleRealTimePayment Merchant Lookup System Error', err);
+            msg.nack();
+            return callback(err);
         }
+        msg.ack();
+        return callback(err, id);
     });
-
-    msg.ack();
 };
-
 
 module.exports = {
 
