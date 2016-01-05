0fedb4f44291f5a4e092496f4117edba50226dd2
Nick Peters <nick@barbetsapp.com>
2016-01-05 11:20:10 -0700

    removed unnecessary node modules from pos-api package.json

--- pos-api/app.js
+++ pos-api/app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var xmlparser = require('express-xml-bodyparser');
 //Routes
@@ -17,7 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 
 //Will catch any JSON syntax issues
@@ -39,7 +36,6 @@ app.use(xmlparser({
 }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/payments', payments);
