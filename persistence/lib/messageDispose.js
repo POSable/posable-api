3cce3db52ffa94efa3741f91727b17dd42a2f00d
David Abramowitz <david.xesllc@gmail.com>
2016-01-06 11:59:37 -0700

    Debugging

--- persistence/lib/messageDispose.js
+++ persistence/lib/messageDispose.js
@@ -1,17 +1,19 @@
 var logPlugin = require('posable-logging-plugin');
 
 var msgDispose = function (msg, err) {
-    if (err && err.deadLetter === true) {
-        logPlugin.debug('msg sent to dead letter que');
-        msg.reject();
-    } else if (err) {
-        logPlugin.debug('msg nacked');
-        // keep track of nack attempts
-        msg.nack();
-    } else {
-        logPlugin.debug('msg acked');
-        msg.ack();
-    }
+    console.log('Here I am');
+        //if (err && err.deadLetter === true) {
+        //    logPlugin.debug('msg sent to dead letter que');
+        //    msg.reject();
+        //} else if (err) {
+        //    logPlugin.debug('msg nacked');
+        //    // keep track of nack attempts
+        //    msg.nack();
+        //} else {
+        //    logPlugin.debug('msg acked');
+        //    msg.ack();
+        //}
+    msg.nack();
 };
 
 module.export = msgDispose;
