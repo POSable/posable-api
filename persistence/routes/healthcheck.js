344de52e90f134a18a2a62138bd8e0b2af66668d
David Abramowitz <david.xesllc@gmail.com>
2016-01-12 15:13:53 -0700

    Remove Healtcheck Test

--- persistence/routes/healthcheck.js
+++ persistence/routes/healthcheck.js
@@ -1,20 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var routerTestWrapper = function () {
+router.get('/', function(req, res) {
+    res.status(200).send('status: 200');
+});
 
-    router.get('/', function (req, res) {
-        res.status(200).send('status: 200');
-    });
-
-}
-
-var testStub = function (testRouter) {
-    router = testRouter;
-}
-
-module.exports = {
-    router: router,
-    routerTestWrapper: routerTestWrapper,
-    testStub: testStub
-};
+module.exports = router;
--- persistence/start.js
+++ persistence/start.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var healthcheck = require('./routes/healthcheck').router;
+var healthcheck = require('./routes/healthcheck');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
