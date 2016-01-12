4cb61e775740f8c14392ffd67738c4a24cec35c9
David Abramowitz <david.xesllc@gmail.com>
2016-01-12 11:22:01 -0700

    complete coverage

--- persistence/handlers/createTransactionPersistence.js
+++ persistence/handlers/createTransactionPersistence.js
@@ -3,45 +3,53 @@ var validate = require('posable-validation-plugin');
 var logPlugin = require('posable-logging-plugin');
 var rabbitDispose = require('../lib/rabbitMsgDispose');
 
+var deadLetterErrorHandling = function (msg, error) {
+    logPlugin.error(error);
+    error.deadLetter = true;
+    rabbitDispose(msg, error);
+};
+
 function createTransactionPersistence(msg) {
     try {
         logPlugin.debug('Received message from Rabbit, Starting Transaction Persistence Handler');
-        var valTransaction = {};
+        var valTransaction;
         var transaction = mapTransaction(msg);
-
-        if (transaction !== undefined) {
+        if (transaction === undefined) {
+            var mapError = new Error('Failed Transaction Persistence Model Mapping');
+            deadLetterErrorHandling(msg, mapError);
+        } else {
             logPlugin.debug('Message Mapped to Model');
             valTransaction = validate.validateTransaction(transaction);
-        } else {
-            var mapError = new Error('Failed Transaction Persistence Model Mapping');
-            logPlugin.error(mapError);
-            mapError.deadLetter = true;
-            rabbitDispose(msg, mapError);
         }
 
-        if (valTransaction && !valTransaction.isValid) {
-            var valError = new Error('Failed Transaction Persistence Validation');
-            logPlugin.error(valError);
-            valError.deadLetter = true;
-            rabbitDispose(msg, valError);
-        } else {
-          logPlugin.debug('Passed Validation and Starting to Save to DB');
+        if (valTransaction && valTransaction.isValid) {
+            logPlugin.debug('Passed Validation and Starting to Save to DB');
             transaction.save(function (err) {
                 if (err) {
-                 logPlugin.error(err);
+                    logPlugin.error(err);
                 } else {
-                 logPlugin.debug('Transaction was saved');
+                    logPlugin.debug('Transaction was saved');
                 }
                 rabbitDispose(msg, err);
             });
+        } else if (valTransaction && !valTransaction.isValid) {
+            var valError = new Error('Failed Transaction Persistence Validation');
+            deadLetterErrorHandling(msg, valError);
         }
     } catch (err) {
-        logPlugin.debug('System Error in Transaction Persistence Handler');
-        logPlugin.error(err);
-        err.deadLetter = true;
-        rabbitDispose(msg, err);
+        deadLetterErrorHandling(msg, err);
         throw err;
     }
 }
 
-module.exports = createTransactionPersistence;
+var testingStub = function (testMapTransaction, testValidate, testLogPlugin, testRabbitDispose) {
+    mapTransaction = testMapTransaction.testMapTransaction;
+    validate = testValidate;
+    logPlugin = testLogPlugin;
+    rabbitDispose = testRabbitDispose.testRabbitDispose;
+};
+
+module.exports = {
+    createTransactionPersistence: createTransactionPersistence,
+    testingStub: testingStub
+};
