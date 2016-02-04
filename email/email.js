23ba10f79ee7652329bf5c955add7539d5c36f8f
Nick Peters <nick@barbetsapp.com>
2016-02-04 12:59:09 -0700

    add test specs to new email/sns service

--- email/email.js
+++ email/email.js
@@ -1,22 +1,29 @@
 var env = require('./common').config();
 var logPlugin = require('posable-logging-plugin');
 var wascallyRabbit = require('posable-wascally-wrapper');
-
 var AWS = require('aws-sdk');
 var sns = new AWS.SNS({region: env['AWS_region']});
 
 
+var formatSNS_msg = function(msg) {
+    var rawRequest = msg.body.data;
+    var errorStatus = msg.body.error;
+    var snsMessage = {
+        Message: 'Error processing post with requestID: ' + msg.properties.correlationId + '.' +
+        '  Error: ' + errorStatus + '.' +
+        '  Raw request: ' + rawRequest,
+        Subject: 'API error for internalID: ' + msg.body.internalID,
+        TopicArn: env['SNS_TopicArn']
+    };
+
+    return snsMessage;
+};
+
+
+// Handler
 var sendMail = function(msg) {
     try {
-        var rawRequest = msg.body.data;
-        var errorStatus = msg.body.error;
-        var snsMessage = {
-            Message: 'Error processing post with requestID: ' + msg.properties.correlationId + '\n' +
-                'Error: ' + errorStatus + '\n' +
-                'Raw request: ' + rawRequest,
-            Subject: 'API error for internalID: ' + msg.body.internalID,
-            TopicArn: env['SNS_TopicArn']
-        };
+        var snsMessage = formatSNS_msg(msg);
 
         if(env['sendSNS'] === true) {
             sns.publish(snsMessage, function(err, data) {
@@ -25,25 +32,32 @@ var sendMail = function(msg) {
                 } else {
                     logPlugin.debug(data);
                 }
-
-                disposeMsg(msg, err);
+                wascallyRabbit.rabbitDispose(msg, err);
             });
         } else {
             logPlugin.debug(snsMessage);
-            disposeMsg(msg, null);
+            wascallyRabbit.rabbitDispose(msg, null);
         }
 
     } catch(err) {
+        err.deadLetter = true;
         logPlugin.error(err);
-        disposeMsg(msg, err);
+        wascallyRabbit.rabbitDispose(msg, err);
     }
-
-
-    function disposeMsg(msg, err) { wascallyRabbit.rabbitDispose(msg, err);}
 };
 
 
+
+var testingStub = function(testDispose, testLogPlugin, testSNS, testENV){
+    wascallyRabbit = testDispose;
+    logPlugin = testLogPlugin;
+    sns = testSNS;
+    env['sendSNS'] = testENV;
+};
+
 module.exports = {
-    sendMail: sendMail
+    sendMail: sendMail,
+    formatSNS_msg: formatSNS_msg,
+    testingStub: testingStub
 };
 
