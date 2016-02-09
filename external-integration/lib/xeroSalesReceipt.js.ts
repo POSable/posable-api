c590e6a90f4a472ce057acbc5a7e087380b1e960
Tristan Rubadeau <tristan@rubadeau.com>
2016-02-09 13:45:15 -0700

    added hard coded Xero SR

---
+++ external-integration/lib/xeroSalesReceipt.js.ts
@@ -0,0 +1,46 @@
+var xero =
+    {
+        "Status": "",
+        "LineItems": [
+            {
+                "TaxType": "NONE",
+                "ItemCode": "Apples",
+                "TaxAmount": 0,
+                "Description": "Apples",
+                "Tracking": [],
+                "AccountCode": "4710",
+                "LineAmount": 1,
+                "Quantity": 1,
+                "UnitAmount": 1,
+                "LineItemID": ""
+            },
+            {
+                "TaxType": "NONE",
+                "ItemCode": "Bacon",
+                "TaxAmount": 0,
+                "Description": "Bacon",
+                "Tracking": [],
+                "AccountCode": "4000",
+                "LineAmount": 2,
+                "Quantity": 1,
+                "UnitAmount": 2,
+                "LineItemID": ""
+            }
+        ],
+        "Reference": "",
+        "HasAttachments": false,
+        "BankAccount": {
+            "AccountID": "8aaeb073-f199-420c-a205-1cbe77864bac",
+            "Name": "Blah Blah Account"
+        },
+        "IsReconciled": false,
+        "SubTotal": 3,
+        "Date": "",
+        "LineAmountTypes": "Exclusive",
+        "CurrencyCode": "USD",
+        "Type": "SPEND",
+        "TotalTax": 0,
+        "Total": 3
+
+    };
+
