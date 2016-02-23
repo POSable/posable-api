ffd6514e275a8b12476023ad3f49e48d956d2f5a
David Abramowitz <david.xesllc@gmail.com>
2016-02-23 14:51:56 -0700

    set headers in response

--- pos-api/lib/pos_modules/sendResponse.js
+++ pos-api/lib/pos_modules/sendResponse.js
@@ -23,15 +23,19 @@ var sendResponse = function (res, statusObject, requestID) {
                 });
             }
         } else {
+            console.log(res);
             if (res.req.headers['content-type'] === 'application/xml') {
                 res.status(statusObject.error.error.code || 400);
+                res.setHeader('content-type', 'application/xml');
+                console.log(res.headers)
                 res.send(o2x({
                     '?xml version="1.0" encoding="utf-8"?': null,
                     status: statusObject.error.error.code,
-                    error: statusObject.error
+                    error: statusObject.error,
                 }));
             } else {
                 res.status(statusObject.error.error.code || 400);
+                res.setHeader('content-type', 'application/json');
                 res.json({
                     error: statusObject.error
                 });
