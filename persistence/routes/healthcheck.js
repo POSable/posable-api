a64c63143f33c36d6f62fcea105bb06145574572
Tristan Rubadeau <tristan@rubadeau.com>
2016-03-09 14:41:54 -0700

    disabled batch query

--- persistence/routes/healthcheck.js
+++ persistence/routes/healthcheck.js
@@ -12,44 +12,44 @@ router.get('/', function(req, res) {
 
     res.status(200).send('status: 200'); // for healthcheck
 
-    logPlugin.debug('Starting batch merchant query');
-    getQueuedMerchants(mongooseMerchantBatchModel, queryCallback);
-
-    function queryCallback(err, merchants) {
-        if (err) {
-            logPlugin.error(err);
-        } else {
-            logPlugin.debug('Finished query');
-            logPlugin.debug('Result : ' + JSON.stringify(merchants));
-            publishBatchRequest(merchants);
-        }
-    }
-
-    function publishBatchRequest(merchants) {
-        try {
-            merchants.forEach(function(merchant){
-                var internalID = merchant.internalID;
-                logPlugin.debug('Sending Batch Command to Rabbit');
-                wascallyRabbit.calculateBatchTotals(internalID, null);
-                persistCompletedBatch(internalID)
-            });
-        } catch (err) {
-            logPlugin.error(err);
-        }
-    }
-
-        function persistCompletedBatch(internalID) {
-            var completedBatch = new CompletedBatch();
-            completedBatch.internalID = internalID;
-            completedBatch.date = new Date();
-            completedBatch.save(function (err) {
-                if (err) {
-                    logPlugin.error(err);
-                } else {
-                    logPlugin.debug('Completed Batch Record was saved');
-                }
-            });
-        }
+    //logPlugin.debug('Starting batch merchant query');
+    //getQueuedMerchants(mongooseMerchantBatchModel, queryCallback);
+    //
+    //function queryCallback(err, merchants) {
+    //    if (err) {
+    //        logPlugin.error(err);
+    //    } else {
+    //        logPlugin.debug('Finished query');
+    //        logPlugin.debug('Result : ' + JSON.stringify(merchants));
+    //        publishBatchRequest(merchants);
+    //    }
+    //}
+    //
+    //function publishBatchRequest(merchants) {
+    //    try {
+    //        merchants.forEach(function(merchant){
+    //            var internalID = merchant.internalID;
+    //            logPlugin.debug('Sending Batch Command to Rabbit');
+    //            wascallyRabbit.calculateBatchTotals(internalID, null);
+    //            persistCompletedBatch(internalID)
+    //        });
+    //    } catch (err) {
+    //        logPlugin.error(err);
+    //    }
+    //}
+    //
+    //    function persistCompletedBatch(internalID) {
+    //        var completedBatch = new CompletedBatch();
+    //        completedBatch.internalID = internalID;
+    //        completedBatch.date = new Date();
+    //        completedBatch.save(function (err) {
+    //            if (err) {
+    //                logPlugin.error(err);
+    //            } else {
+    //                logPlugin.debug('Completed Batch Record was saved');
+    //            }
+    //        });
+    //    }
 });
 
 module.exports = router;
