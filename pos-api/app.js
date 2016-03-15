cdfd61a6616f09087163e7f761472ccc1ea48f9b
Nick Peters <nick@barbetsapp.com>
2016-03-15 11:05:46 -0600

    updated mapping for new full transactions

--- pos-api/app.js
+++ pos-api/app.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var xmlparser = require('express-xml-bodyparser');
+require('body-parser-xml')(bodyParser);
 //Routes
 var healthcheck = require('./routes/healthcheck');
-//var payments = require('./routes/payments');
 var transactions = require('./routes/transactions');
 var fullTransactions = require('./routes/fullTransactions');
-//var errorHandling = require('./lib/pos_modules/errorHandling');
 
 var app = express();
 app.use(express.static('public'));
@@ -16,7 +14,9 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 app.use(bodyParser.json());
+app.use(bodyParser.xml({ xmlParseOptions: { explicitArray: false, trim: true }}));
 
 //Will catch any JSON syntax issues
 
@@ -28,16 +28,6 @@ app.use(function (error, req, res, next) {
     }
     next(error);
 });
-try {
-    app.use(xmlparser({
-        explicitArray: false,
-        normalize: false,
-        normalizeTags: false,
-        trim: true
-    }));
-} catch (err) {
-    console.log(err);
-}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -46,13 +36,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/transactions', transactions);
 app.use('/healthcheck', healthcheck);
 app.use('/fullTransactions', fullTransactions);
-
-// catch 404 and forward to error handler - this crashed Elastic Beanstalk!!!
-//app.use(function(req, res, next) {
-//  var err = new Error('Not Found');
-//    err.status = 404;
-//    errorHandling(err, res);
-//    next(err);
-//});
 
 module.exports = app;
