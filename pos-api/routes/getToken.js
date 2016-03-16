ae126a5d6bccbab603d6e955e2168495ccc2a8ab
David Abramowitz <david.xesllc@gmail.com>
2016-03-16 15:10:57 -0600

    getToken Tests and Code

--- pos-api/routes/getToken.js
+++ pos-api/routes/getToken.js
@@ -1,13 +1,13 @@
 /**
  * Created by davidabramowitz on 3/14/16.
  */
-
 // 3rd Party Plugins
 var express = require('express');
 var uuid = require('node-uuid');
-var jwt = require('jsonwebtoken');
+var o2x = require('object-to-xml');
 // POSable Plugins
 var logPlugin = require('posable-logging-plugin');
+var configPlugin = require('posable-customer-config-plugin')();
 // Modules
 var sendResponse = require('../lib/pos_modules/sendResponse');
 var checkErrorAltResponsePath = require('../lib/pos_modules/checkErrorAltResponsePath').checkErrorAltResponsePath;
@@ -16,7 +16,7 @@ var reqHeaderTokenProvider = require ('../lib/pos_modules/api/reqHeaderTokenProv
 // Var Extraction
 var router = express.Router();
 
-router.post('/', function(req, res) {
+var postGetToken = function(req, res) {
     var requestID = uuid.v4();
     logPlugin.debug(requestID + " - get Token received with content type of" + " " + req.headers['content-type']);
     var statusObject = {isOK: true, success: []};
@@ -26,7 +26,6 @@ router.post('/', function(req, res) {
     if (statusObject.isOK) {
         checkPostToken(jwtokenRequest, statusObject, checkPostTokenCallback);
     } else {
-        checkErrorAltResponsePath(req, statusObject);
         sendResponse(res, statusObject, requestID);
     }
 
@@ -38,20 +37,44 @@ router.post('/', function(req, res) {
                 error: {code: 500, message: "System Error with Token Authentication"}
             };
         }
-
+        configPlugin.getToken(req.body.GetToken, configPluginCallback);
     }
-
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var name = firstName + lastName
-
-
-    var options =  {}
-    var payload = {
-        "name": name,
-        "internalID": internalID,
+    function configPluginCallback(err, token) {
+        if (err) {
+            logPlugin.error(err);
+            logPlugin.debug('getToken finished and did not return a token ');
+            res.set('Content-Type', 'application/xml');
+            res.status(400);
+            return  res.send(o2x({
+                '?xml version="1.0" encoding="utf-8"?': null,
+                status: 400,
+                token: "N/A",
+                message: "Internal Error - Token was not generated."
+            }));
+        } else {
+            logPlugin.debug('getToken finished and returned a token ');
+            statusObject.token = token;
+            res.set('Content-Type', 'application/xml');
+            return  res.send(o2x({
+                '?xml version="1.0" encoding="utf-8"?': null,
+                token: token,
+            }));
+        }
     }
+};
 
-});
+router.post('/', postGetToken);
 
-module.exports = router;
+var testingStub = function (testLogPlugin, testConfigPlugin, testSendResponse, testCheckPostToken, testReqHeaderTokenProvider) {
+    logPlugin = testLogPlugin;
+    configPlugin = testConfigPlugin;
+    sendResponse = testSendResponse;
+    checkPostToken = testCheckPostToken;
+    reqHeaderTokenProvider = testReqHeaderTokenProvider;
+};
+
+module.exports = {
+    router: router,
+    postGetToken : postGetToken,
+    testingStub: testingStub
+}
