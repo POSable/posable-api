44059c9d11b2ea61cb872961603b03e6ab13ca63
Tristan Rubadeau <tristan@rubadeau.com>
2016-03-17 12:05:39 -0600

    refactor to invoicePersistence function flow

--- external-integration/lib/invoicePersistence.js
+++ external-integration/lib/invoicePersistence.js
@@ -8,10 +8,10 @@ var populateInvoice = function(invoice, id, batchTime) {
     invoice.internalID = id;
     invoice.finalizeAt = batchTime;
     invoice.invoiceItems = [];
-    return invoice
 };
 var addSaleItem = function(msg, invoice) {
     if(msg.body.data.amount > 0) {
+        var saleInvoiceItem = new InvoiceItem();
         saleInvoiceItem.transactionID = msg.body.data.transactionID;
         saleInvoiceItem.type = "sale";
         saleInvoiceItem.amount = msg.body.data.amount;
@@ -20,6 +20,7 @@ var addSaleItem = function(msg, invoice) {
 };
 var addTaxItem = function(msg, invoice) {
     if(msg.body.data.tax > 0) {
+        var taxInvoiceItem = new InvoiceItem();
         taxInvoiceItem.transactionID = msg.body.data.transactionID;
         taxInvoiceItem.type = "tax";
         taxInvoiceItem.amount = msg.body.data.tax;
@@ -28,22 +29,30 @@ var addTaxItem = function(msg, invoice) {
 };
 var addDiscountItem = function(msg, invoice) {
     if(msg.body.data.amount > 0) {          //need dataModel    <---------------------- Need to map at POS-API level
+        var discountInvoiceItem = new InvoiceItem();
         discountInvoiceItem.transactionID = msg.body.data.transactionID;
-        discountInvoiceItem.type = "tax";
+        discountInvoiceItem.type = "discount";
         discountInvoiceItem.amount = msg.body.data.amount; //need dataModel
         invoice.invoiceItems.push(discountInvoiceItem);
     }
 };
 var addGiftCardItem = function(msg, invoice) {
     if(msg.body.data.paymentType === 'gift') {
+        var giftCardInvoiceItem = new InvoiceItem();
         giftCardInvoiceItem.transactionID = msg.body.data.transactionID;
         giftCardInvoiceItem.type = "giftCard";
         giftCardInvoiceItem.amount = msg.body.data.amount; //need dataModel
         invoice.invoiceItems.push(giftCardInvoiceItem);
     }
 };
-var invoiceSaveAndMsgDispose = function(msg, invoice) {
-    invoice.save(function (err) {
+var addInvoiceItems = function(msg, foundInvoice) {
+    addSaleItem(msg, foundInvoice);
+    addTaxItem(msg, foundInvoice);
+    addDiscountItem(msg, foundInvoice);
+    addGiftCardItem(msg, foundInvoice);
+};
+var invoiceSaveAndMsgDispose = function(msg, foundInvoice) {
+    foundInvoice.save(function (err) {
         if (err) {
             logPlugin.error(err);
         } else {
@@ -58,45 +67,26 @@ var invoicePersistence = function(msg, merchant) {
         logPlugin.debug('Starting InvoiceItem Mapping');
 
         var foundInvoice = null;
-        var finalizeAt = Date.now();
+        var finalizeAt = Date.now(); //if real time, never gets over-ridden
         var id = merchant.internalID;
         var batchTime = merchant.batchTime;
 
-        var invoice = new Invoice();
-
         if(merchant.batchType === "batch") {
+            finalizeAt = batchTime; //override this to merchant's batch time so it stays open
 
-            foundInvoice = null; // Write the find query
-
-            if(!foundInvoice) {
-                populateInvoice(invoice, id, batchTime);
-                addSaleItem(msg, invoice);
-                addTaxItem(msg, invoice);
-                addDiscountItem(msg, invoice);
-                addGiftCardItem(msg, invoice);
-                invoiceSaveAndMsgDispose(msg, invoice)
-            } else {
-                //procedure for existing Invoice that needs InvoiceItems added to it
-                addSaleItem(msg, foundInvoice);
-                addTaxItem(msg, foundInvoice);
-                addDiscountItem(msg, foundInvoice);
-                addGiftCardItem(msg, foundInvoice);
-                wascallyRabbit.rabbitDispose(msg, null);
-            }
-
-        } if(merchant.batchType === "real-time") {
-            populateInvoice(invoice, id, finalizeAt);
-            addSaleItem(msg, invoice);
-            addTaxItem(msg, invoice);
-            addDiscountItem(msg, invoice);
-            addGiftCardItem(msg, invoice);
-            invoiceSaveAndMsgDispose(msg, invoice)
-
-        } else {
-            logPlugin.debug('batchType was not found in Invoice Persistence for InternalID : ', merchant.internalID);
-            logPlugin.debug('Disposing of Msg');
-            wascallyRabbit.rabbitDispose(msg, null);
+            //if batch merchant's invoice already open for day, edit existing
+            foundInvoice = null; // Write the find query....This will be async and need to be put into a callback...
         }
+
+        if (!foundInvoice) {
+            //either realtime merch or batch merch's first transaction
+            foundInvoice = new Invoice();
+            populateInvoice(foundInvoice, id, finalizeAt);
+        }
+
+        //set all line item functions on either new or found (db) invoice
+        addInvoiceItems(msg, foundInvoice);
+        invoiceSaveAndMsgDispose(msg, foundInvoice);
 
     } catch (err) {
         logPlugin.debug('System error in invoicePersistence');
