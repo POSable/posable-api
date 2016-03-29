804fcd48c9eb7841b524bf1c36119c4a7eb1649c
Tristan Rubadeau <tristan@rubadeau.com>
2016-03-29 16:26:52 -0600

    added npm forEachAsync

--- external-integration/lib/paymentJob/paymentReceiptProcedure.js
+++ external-integration/lib/paymentJob/paymentReceiptProcedure.js
@@ -4,12 +4,12 @@ var logPlugin = require('posable-logging-plugin');
 var paymentReceiptMap = require('./paymentReceiptMap');
 var invoiceMerchantSearch = require('../common/merchantSearch');
 var paymentQuery = require('./paymentQuery');
+var forEachAsync = require('forEachAsync').forEachAsync;
 
 var paymentReceiptProcedure = function (paymentsArray, qbInvoiceID, merchConfig) {
     // This is kicked off by the successful Invoice Post Procedure and Payment Query
     try {
-
-        paymentsArray.forEach(function(payment) {
+        forEachAsync(paymentsArray, function(next, payment) {
             var paymentReceipt = paymentReceiptMap(merchConfig, qbInvoiceID, payment);
 
             postPaymentProcedure(merchConfig, paymentReceipt, function(err, qbPaymentID) {
@@ -17,8 +17,12 @@ var paymentReceiptProcedure = function (paymentsArray, qbInvoiceID, merchConfig)
                     logPlugin.error(err);
                 } else {
                     logPlugin.debug('ExternalPost of Payment: ' + qbPaymentID + ' Posted and Updated successfully');
+                    //logPlugin.debug('Async fun');
+                    next();
                 }
             });
+        }).then(function(){
+            logPlugin.debug('All Done with forEachAsync Posting');
         });
 
     } catch (err) {
