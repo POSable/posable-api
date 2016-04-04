e217ed2a52fa16573048ae82e3406df91163fc08
Nick Peters <nick@barbetsapp.com>
2016-04-04 12:37:30 -0600

    add timer to post extPosts for refunds

--- external-integration/lib/cloudElements/CloudElemTimer.js
+++ external-integration/lib/cloudElements/CloudElemTimer.js
@@ -1,12 +1,14 @@
 var logPlugin = require('posable-logging-plugin');
 var extPostInvoiceQuery = require('./invoices/extPostInvoiceQuery');
 var extPostPaymentQuery = require('./payments/extPostPaymentQuery');
+var extPostRefundQuery = require('./refunds/extPostRefundQuery');
 
 var timer = function() {
     try {
         function kickoff() {
             extPostInvoiceQuery();
             extPostPaymentQuery();
+            extPostRefundQuery();
         }
         setInterval(function(){ kickoff() }, 30000);
 
